d98dae800a60ef683d86f2a1e7c54930226f0f5c
Yongqi Wang <wangyq977@gmail.com>
2021-03-17 14:40:18 +0100

    [my] Fix issues with Apple silicon and brew
    * Add Dock position and hot corners in perferences
    * Change Homebrew install function
    * Suppress zsh warning
    * Export `/opt/homebrew/bin` for homebrew install
    * Change repo name

--- src/os/preferences/macos/dock.sh
+++ src/os/preferences/macos/dock.sh
@@ -50,4 +50,28 @@ execute "defaults write com.apple.dock showhidden -bool true" \
 execute "defaults write com.apple.dock tilesize -int 60" \
     "Set icon size"
 
+execute "defaults write com.apple.dock orientation left" \
+    "Set Dock in left orientation"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setting hot corners
+
+# Possible values:
+#  0: no-op
+#  2: Mission Control (all windows)
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+execute "defaults write com.apple.dock wvous-tl-corner -int 2 && defaults write com.apple.dock wvous-tl-modifier -int 0" \
+    "Set Top Left to Mission Control (all windows)"
+
+execute "defaults write com.apple.dock wvous-bl-corner -int 11 && defaults write com.apple.dock wvous-bl-modifier -int 0" \
+    "Set Bottom Left to Launchpad"
+
 killall "Dock" &> /dev/null
