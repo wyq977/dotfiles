ac80acfe822192e352801f612ab6b9a6e8bbd90f
sdsadfaf <wyq977@users.noreply.github.com>
2023-02-01 20:48:54 +0100

    os: remove .bash.local

--- src/os/create_local_config_files.sh
+++ src/os/create_local_config_files.sh
@@ -5,37 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-create_bash_local() {
-
-    declare -r FILE_PATH="$HOME/.bash.local"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-   if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
-
-        DOTFILES_BIN_DIR="$(dirname "$(pwd)")/bin/"
-
-        printf "%s\n" \
-"#!/bin/bash
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Set PATH additions.
-
-PATH=\"$DOTFILES_BIN_DIR:\$PATH\"
-
-export PATH
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-" \
-        >> "$FILE_PATH"
-   fi
-
-    print_result $? "$FILE_PATH"
-
-}
-
 create_gitconfig_local() {
 
     declare -r FILE_PATH="$HOME/.gitconfig.local"
@@ -71,7 +40,6 @@ main() {
 
     print_in_purple "\n • Create local config files\n\n"
 
-    create_bash_local
     create_gitconfig_local
 
 }
